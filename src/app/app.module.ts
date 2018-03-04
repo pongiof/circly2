d446653a8ab61275e0d0dc1472918194f7f08bcf
Francesco Pongetti <fpongetti@fpongetti-macbookpro1.roam.corp.google.com>
2018-03-04 13:48:59 +0000

    Adding Angular Material, Animations, Gestures support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CirclyMaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CirclyMaterialModule,
     AppRoutingModule
   ],
   providers: [],
